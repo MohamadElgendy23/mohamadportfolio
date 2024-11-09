63748077476720113dc0deefe0b0da66712eac4c
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2024-11-09 10:36:55 -0700

    contact page setup and projects edits.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,65 @@
 import React from "react";
-import Nav from "/Users/mohamadelgendy/Desktop/mohamadportfolio/src/components/Nav/Nav.js";
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
+
 function Projects() {
   return (
     <div className="Projects">
       <Nav></Nav>
       <div className="flex items-center justify-center flex-col font-mono mt-7">
-        <h1>Projects</h1>
+        <h1 className="font-bold italic text-2xl mb-8">
+          Projects{" "}
+          <span className="not-italic">
+            <i className="fa-solid fa-clipboard"></i>
+          </span>
+        </h1>
+        <div className="flex items-center justify-center flex-col gap-10">
+          <div>
+            <h2 className="font-bold mb-3">Hospital Management Application</h2>
+            <span className="text-2xl text-blue-800">&#63;</span>
+            <p className="text-center">
+              MERN and Bootstrap application where users can view available{" "}
+              <br />
+              appointments, view doctors on call, and view patients waiting in
+              line.
+            </p>
+            <p className="text-center pt-5">
+              <span className="italic">Github Link: </span>
+              https://github.com/MohamadElgendy23/HospitalManagementApp
+            </p>
+          </div>
+          <div>
+            <h2 className="font-bold mb-3">Roster Viewer</h2>
+            <span className="text-2xl text-blue-800">&#63;</span>
+            <p className="text-center">
+              Aesthetic application created in React.js, JS, HTML, and CSS where{" "}
+              <br />
+              users can view available NBA teams and each team's roster.
+            </p>
+            <p className="text-center pt-5">
+              <span className="italic">Github Link: </span>
+              https://github.com/MohamadElgendy23/rosterviewer <br />
+              <span className="italic">Vercel Link: </span>
+              https://rosterviewerdeploy.vercel.app
+            </p>
+          </div>
+          <div>
+            <h2 className="font-bold mb-3">
+              Flixforall - All in One Movie App
+            </h2>
+            <span className="text-2xl text-blue-800">&#63;</span>
+            <p id="project-description" className="text-center">
+              MERN application where users can view movies and <br />
+              add/edit/view/delete reviews.
+            </p>
+            <p className="text-center pt-5">
+              <span className="italic">Github Link: </span>
+              https://github.com/MohamadElgendy23/flix-for-all <br />
+            </p>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Nav from "/Users/mohamadelgendy/Desktop/mohamadportfolio/src/components/Nav/Nav.js";
+import Footer from "../Footer/Footer";
 function Resume() {
   return (
     <div className="Resume">
       <Nav></Nav>
       <div className="flex items-center justify-center flex-col font-mono mt-7">
-        <h1>Resume</h1>
+        <h1 className="font-bold italic text-2xl mb-8">
+          Resume <span className="not-italic"></span>
+        </h1>
       </div>
+      <Footer />
     </div>
   );
 }
