56e9b7301448c3491ee964fab5bf35801f2fe989
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2024-11-17 19:12:09 -0700

    will build out resume page as component and allow it to be displayed as its on the page

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,34 +2,21 @@ import React, { useState } from "react";
 import Nav from "../Nav/Nav";
 import "./Resume.css";
 function Resume() {
-  const [isVisible, setIsVisible] = useState(false);
-
-  function viewResume(event) {
-    event.preventDefault();
-    setIsVisible(!isVisible);
+  function ResumePage() {
+    <div className=""></div>;
   }
 
   return (
     <div className="Resume">
       <Nav></Nav>
-      <div className="flex items-center justify-center flex-col font-mono mt-7">
+      <div className="flex items-center justify-center flex-col font-mono mt-7 gap-5">
         <h1 className="font-bold italic text-2xl mb-8">
           Resume{" "}
           <span className="not-italic">
             <i className="fas fa-file-alt"></i>
           </span>
         </h1>
-        <a
-          href="/resume"
-          id="viewResume"
-          className="text-blue-500 underline"
-          onClick={viewResume}
-        >
-          View My Resume
-        </a>
-        <div className={`resumeImage ${isVisible ? "visible" : ""}`}>
-          <img src="../images/Resume.jpeg" alt="resume"></img>
-        </div>
+        <ResumePage />
       </div>
     </div>
   );
